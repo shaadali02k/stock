e007c9c7c14968b639ba7dcd63edf1ffd1305480
shaadali02k <shaadali02k@gmail.com>
2025-12-19 12:31:18 +0530

    changes updated

--- Project/stock.py
+++ Project/stock.py
@@ -5,3 +5,10 @@ if num % 2 == 0:
 else:
     print("Odd number")
 
+import random
+import string
+
+password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
+print("Generated Password:", password)
+
+
